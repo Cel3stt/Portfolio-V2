e5135a4b961b74dac959fee5032f179c7fb4d785
Cel3stt <mariannecelest@gmail.com>
2026-04-21 00:13:09 +0800

    Add experience schema type with fields for company name, job title, start date, end date, location, and job description

---
+++ sanity/schemaTypes/experience.ts
@@ -0,0 +1,45 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+    name: 'experience',
+    title: 'Experience',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'companyName',
+            title: 'Company Name',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'jobTitle',
+            title: 'Job Title',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'startDate',
+            title: 'Start Date',    
+            type: 'date',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'endDate',
+            title: 'End Date',
+            type: 'date',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'companyLocation',
+            title: 'Company Location',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'jobDescription',
+            title: 'Job Description',
+            type: 'text',
+            validation: (Rule) => Rule.required(),
+        }),
+    ]
+})
