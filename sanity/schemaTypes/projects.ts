014da6a186adccc9d02f188d89946e3f9acf6f2a
Cel3stt <mariannecelest@gmail.com>
2026-04-21 00:09:09 +0800

    Add project schema type with fields for name, description, link, and tech stack

---
+++ sanity/schemaTypes/projects.ts
@@ -0,0 +1,64 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'projectName',
+            title: 'Project Name',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'projectDescription',
+            title: 'Project Description',
+            type: 'object',
+             fields: [
+    {
+      name: 'overview',
+      title: 'Overview',
+      type: 'text',
+    },
+    {
+      name: 'features',
+      title: 'Features',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      name: 'techStack',
+      title: 'Tech Stack',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+        name: 'projectImage',
+        title: 'Project Image',
+        type: 'image',
+    }
+  ],
+            
+        }),
+        defineField({
+            name: 'projectLink',
+            title: 'Project Link',
+            type: 'object',
+            fields: [
+                {
+                    name: 'projectLabel',
+                    title: 'Project Label',
+                    type: 'string',
+                },
+                {
+                    name: 'projectURL',
+                    title: 'Project URL',
+                    type: 'url',
+                    validation: (Rule) => Rule.uri({scheme: ['http', 'https']})
+
+                }
+            ]
+        })
+    ]
+})
